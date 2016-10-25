c5195e2cb7f2456044341d5236e091874414cafa
Dylan McDonald <dmcdonald21@cnm.edu>
2016-10-25 09:10:25 -0600

    added existing content

---
+++ public_html/github.php
@@ -0,0 +1,20 @@
+<?php
+require_once(dirname(__DIR__) . "/vendor/autoload.php");
+require_once(dirname(__DIR__) . "/php/classes/autoload.php");
+use GuzzleHttp\Client;
+use Edu\Cnm\GitHubBrowser\GitHubException;
+
+$guzzle = new Client(["base_uri" => "https://api.github.com/repos/dylan-mcdonald/angular2-diceware/"]);
+$reply = $guzzle->get("branches/master");
+if($reply->getStatusCode() < 200 || $reply->getStatusCode() >= 300) {
+	throw(new GitHubBrowserException("unable to contact github: " . $reply->getReasonPhrase(), $reply->getStatusCode()));
+}
+
+$json = (string)$reply->getBody();
+$repositoryBase = json_decode($json);
+$sha = $repositoryBase->commit->sha;
+
+$reply = $guzzle->get("git/trees/$sha?recursive=1");
+$json = (string)$reply->getBody();
+$repositoryTree = json_decode($json);
+var_dump($repositoryTree);
