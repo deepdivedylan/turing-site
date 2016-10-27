f35bf6eaf80aeb006ddacbb385c7dc304b07c04d
Dylan McDonald <dmcdonald21@cnm.edu>
2016-10-26 22:27:16 -0600

    finalized comparable

--- php/classes/GitHubEntity.php
+++ php/classes/GitHubEntity.php
@@ -2,57 +2,57 @@
 namespace Edu\Cnm\GitHubBrowser;
 
 /**
- * Container for GitHub API Result
- *
- * Container for the GitHub Tree result, which performs a depth first search of a repository's tree or subtree
- *
- * @author Dylan McDonald <dmcdonald21@cnm.edu>
- * @see https://developer.github.com/v3/git/trees/ GitHub Trees
- **/
+* Container for GitHub API Result
+*
+* Container for the GitHub Tree result, which performs a depth first search of a repository's tree or subtree
+*
+* @author Dylan McDonald <dmcdonald21@cnm.edu>
+* @see https://developer.github.com/v3/git/trees/ GitHub Trees
+**/
 class GitHubEntity implements \JsonSerializable {
 	/**
-	 * path relative to the root of repository
-	 * @var string $path
-	 **/
+	* path relative to the root of repository
+	* @var string $path
+	**/
 	private $path;
 	/**
-	 * permissions for the entity
-	 * @var int $mode
-	 **/
+	* permissions for the entity
+	* @var int $mode
+	**/
 	private $mode;
 	/**
-	 * SHA1 signature for the entity, as a string of hexits
-	 * @var string $sha
-	 **/
+	* SHA1 signature for the entity, as a string of hexits
+	* @var string $sha
+	**/
 	private $sha;
 	/**
-	 * file size of this entity, in bytes
-	 * @var int $size
-	 **/
+	* file size of this entity, in bytes
+	* @var int $size
+	**/
 	private $size;
 	/**
-	 * type of entity; can be one of blob, commit, or tree
-	 * @var string $type
-	 **/
+	* type of entity; can be one of blob, commit, or tree
+	* @var string $type
+	**/
 	private $type;
 	/**
-	 * URL for this entity
-	 * @var string $url
-	 **/
+	* URL for this entity
+	* @var string $url
+	**/
 	private $url;
 
 	/**
-	 * constructor for this entity
-	 *
-	 * the parameters may be passed as one of two formats:
-	 * - object: {"path" : $newPath, "mode" : $newMode, "sha" : $newSha, "size" : $newSize, "type" : $newType, "url" : $newUrl}
-	 * - parameters one at time, mirroring the array above
-	 * if the parameters are not passed in either format, a BadMethodCallException is thrown
-	 *
-	 * @throws \BadMethodCallException if the constructor is called in an invalid manner
-	 * @throws \InvalidArgumentException if strings are insecure or empty
-	 * @throws \RangeException if integers are out of range
-	 **/
+	* constructor for this entity
+	*
+	* the parameters may be passed as one of two formats:
+	* - object: {"path" : $newPath, "mode" : $newMode, "sha" : $newSha, "size" : $newSize, "type" : $newType, "url" : $newUrl}
+	* - parameters one at time, mirroring the array above
+	* if the parameters are not passed in either format, a BadMethodCallException is thrown
+	*
+	* @throws \BadMethodCallException if the constructor is called in an invalid manner
+	* @throws \InvalidArgumentException if strings are insecure or empty
+	* @throws \RangeException if integers are out of range
+	**/
 	public function __construct() {
 		$parameters = ["path", "mode", "sha", "size", "type", "url"];
 		$numArgs = func_num_args();
@@ -91,47 +91,47 @@ class GitHubEntity implements \JsonSerializable {
 	}
 
 	/**
-	 * evalulates whether this is a directory
-	 *
-	 * @return bool true if a directory, false if not
-	 **/
+	* evalulates whether this is a directory
+	*
+	* @return bool true if a directory, false if not
+	**/
 	public function isDirectory() {
 		return($this->type === "tree");
 	}
 
 	/**
-	 * evalulates whether this is a file
-	 *
-	 * @return bool true if a file, false if not
-	 **/
+	* evalulates whether this is a file
+	*
+	* @return bool true if a file, false if not
+	**/
 	public function isFile() {
 		return($this->type === "blob");
 	}
 
 	/**
-	 * calculates and returns the directory depth
-	 *
-	 * @return int directory depth
-	 **/
+	* calculates and returns the directory depth
+	*
+	* @return int directory depth
+	**/
 	public function getDirectoryDepth() : int {
 		return(substr_count($this->path, "/"));
 	}
 
 	/**
-	 * accessor method for path
-	 *
-	 * @return string current value of path
-	 **/
+	* accessor method for path
+	*
+	* @return string current value of path
+	**/
 	public function getPath() : string {
 		return($this->path);
 	}
 
 	/**
-	 * mutator method for path
-	 *
-	 * @param string $newPath new value of path
-	 * @throws \InvalidArgumentException if path is invalid
-	 **/
+	* mutator method for path
+	*
+	* @param string $newPath new value of path
+	* @throws \InvalidArgumentException if path is invalid
+	**/
 	public function setPath(string $newPath) {
 		$newPath = trim($newPath);
 		$newPath = filter_var($newPath, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
@@ -144,20 +144,20 @@ class GitHubEntity implements \JsonSerializable {
 	}
 
 	/**
-	 * accessor method for mode
-	 *
-	 * @return int current value of mode
-	 **/
+	* accessor method for mode
+	*
+	* @return int current value of mode
+	**/
 	public function getMode() : int {
 		return($this->mode);
 	}
 
 	/**
-	 * mutator method for mode
-	 *
-	 * @param int|string $newMode new value of mode
-	 * @throws \InvalidArgumentException if mode is invalid
-	 **/
+	* mutator method for mode
+	*
+	* @param int|string $newMode new value of mode
+	* @throws \InvalidArgumentException if mode is invalid
+	**/
 	public function setMode($newMode) {
 		if(gettype($newMode) === "integer") {
 			$this->mode = $newMode;
@@ -174,20 +174,20 @@ class GitHubEntity implements \JsonSerializable {
 	}
 
 	/**
-	 * accessor method for sha
-	 *
-	 * @return string current value of sha
-	 **/
+	* accessor method for sha
+	*
+	* @return string current value of sha
+	**/
 	public function getSha() : string {
 		return($this->sha);
 	}
 
 	/**
-	 * mutator method for sha
-	 *
-	 * @param string $newSha new value of sha
-	 * @throws \InvalidArgumentException if sha is invalid
-	 **/
+	* mutator method for sha
+	*
+	* @param string $newSha new value of sha
+	* @throws \InvalidArgumentException if sha is invalid
+	**/
 	public function setSha(string $newSha) {
 		$newSha = trim($newSha);
 		if(ctype_xdigit($newSha) === false) {
@@ -201,20 +201,20 @@ class GitHubEntity implements \JsonSerializable {
 	}
 
 	/**
-	 * accessor method for size
-	 *
-	 * @return string current value of size
-	 **/
+	* accessor method for size
+	*
+	* @return string current value of size
+	**/
 	public function getSize() : int {
 		return($this->size);
 	}
 
 	/**
-	 * mutator method for size
-	 *
-	 * @param string $newSize new value of size
-	 * @throws \RangeException if size is invalid
-	 **/
+	* mutator method for size
+	*
+	* @param string $newSize new value of size
+	* @throws \RangeException if size is invalid
+	**/
 	public function setSize(int $newSize = null) {
 		if($newSize === null) {
 			$this->size = null;
@@ -229,20 +229,20 @@ class GitHubEntity implements \JsonSerializable {
 	}
 
 	/**
-	 * accessor method for type
-	 *
-	 * @return string current value of type
-	 **/
+	* accessor method for type
+	*
+	* @return string current value of type
+	**/
 	public function getType() : string {
 		return($this->type);
 	}
 
 	/**
-	 * mutator method for type
-	 *
-	 * @param string $newType new value of type
-	 * @throws \InvalidArgumentException if type is invalid
-	 **/
+	* mutator method for type
+	*
+	* @param string $newType new value of type
+	* @throws \InvalidArgumentException if type is invalid
+	**/
 	public function setType(string $newType) {
 		$newType = trim($newType);
 		$validTypes = ["blob", "commit", "tree"];
@@ -255,20 +255,20 @@ class GitHubEntity implements \JsonSerializable {
 	}
 
 	/**
-	 * accessor method for url
-	 *
-	 * @return string current value of url
-	 **/
+	* accessor method for url
+	*
+	* @return string current value of url
+	**/
 	public function getUrl() : string {
 		return($this->url);
 	}
 
 	/**
-	 * mutator method for url
-	 *
-	 * @param string $newUrl new value of url
-	 * @throws \InvalidArgumentException if url is invalid
-	 **/
+	* mutator method for url
+	*
+	* @param string $newUrl new value of url
+	* @throws \InvalidArgumentException if url is invalid
+	**/
 	public function setUrl(string $newUrl) {
 		$newUrl = trim($newUrl);
 		$newUrl = filter_var($newUrl, FILTER_SANITIZE_URL);
@@ -281,10 +281,10 @@ class GitHubEntity implements \JsonSerializable {
 	}
 
 	/**
-	 * formats the state variables for JSON serialization
-	 *
-	 * @return array resulting state variables to serialize
-	 **/
+	* formats the state variables for JSON serialization
+	*
+	* @return array resulting state variables to serialize
+	**/
 	public function jsonSerialize() : array {
 		$fields = [];
 		$fields["depth"] = $this->getDirectoryDepth();
@@ -293,16 +293,28 @@ class GitHubEntity implements \JsonSerializable {
 	}
 
 	/**
-	 * comparison method for two GitHubEntities
-	 *
-	 * @param GitHubEntity $otherEntity entity to compare to
-	 * @return int negative, zero, or positive if less than, equal to, or greater than the other entity
-	 **/
-	public static function compareTo(GitHubEntity $thisEntity,GitHubEntity $thatEntity) {
+	* comparison method for two GitHubEntities
+	*
+	* @param GitHubEntity $otherEntity entity to compare to
+	* @return int negative, zero, or positive if less than, equal to, or greater than the other entity
+	**/
+	public static function compareTo(GitHubEntity $thisEntity, GitHubEntity $thatEntity) {
 		if($thisEntity->getDirectoryDepth() !== $thatEntity->getDirectoryDepth()) {
 			return($thisEntity->getDirectoryDepth() - $thatEntity->getDirectoryDepth());
 		} else {
-			return(strcmp($thisEntity->getPath(), $thatEntity->getPath()));
+			if($thisEntity->isDirectory() === true) {
+				if($thatEntity->isDirectory() === true) {
+					return(strcmp($thisEntity->getPath(), $thatEntity->getPath()));
+				} else {
+					return(-1);
+				}
+			} else {
+				if($thatEntity->isDirectory() === true) {
+					return(1);
+				} else {
+					return(strcmp($thisEntity->getPath(), $thatEntity->getPath()));
+				}
+			}
 		}
 	}
 }
