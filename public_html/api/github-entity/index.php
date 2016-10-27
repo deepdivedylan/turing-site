d33f9156d07d082bb258a593b31ea13ca46b5314
Dylan McDonald <dmcdonald21@cnm.edu>
2016-10-26 22:36:23 -0600

    added JSON API

---
+++ public_html/api/github-entity/index.php
@@ -0,0 +1,47 @@
+<?php
+require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 3) . "/php/classes/autoload.php");
+use GuzzleHttp\Client;
+use Edu\Cnm\GitHubBrowser\{GitHubEntity, GitHubException};
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	$guzzle = new Client(["base_uri" => "https://api.github.com/repos/dylan-mcdonald/angular2-diceware/"]);
+	$reply = $guzzle->get("branches/master");
+	if($reply->getStatusCode() < 200 || $reply->getStatusCode() >= 300) {
+		throw(new GitHubBrowserException("unable to contact github: " . $reply->getReasonPhrase(), $reply->getStatusCode()));
+	}
+
+	$json = (string)$reply->getBody();
+	$repositoryBase = json_decode($json);
+	$sha = $repositoryBase->commit->sha;
+
+	$reply = $guzzle->get("git/trees/$sha?recursive=1");
+	$json = (string)$reply->getBody();
+	$repositoryTree = json_decode($json)->tree;
+
+	$gitHubEntities = [];
+	foreach($repositoryTree as $entity) {
+		$gitHubEntities[] = new GitHubEntity($entity);
+	}
+	usort($gitHubEntities, ["Edu\\Cnm\\GitHubBrowser\\GitHubEntity", "compareTo"]);
+	$reply->data = $gitHubEntities;
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
+
+header("Content-type: application/json");
+
+// encode and return reply to front end caller
+echo json_encode($reply);
