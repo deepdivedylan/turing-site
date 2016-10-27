df13b49dfe74e9a2d744b2b080d82fec6d6bddb4
Dylan McDonald <dmcdonald21@cnm.edu>
2016-10-26 23:23:30 -0600

    added main app

--- public_html/github/index.php
+++ public_html/github/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html ng-app="GitHubBrowser">
 	<head>
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
+
+		<script type="text/javascript" src="github-browser.js"></script>
 		<title>GitHub Browser</title>
 	</head>
 	<body>
