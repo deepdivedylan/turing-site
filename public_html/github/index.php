3831749b4b74bd503b39be603e3514e363e5dffc
Dylan McDonald <dmcdonald21@cnm.edu>
2016-10-26 23:21:16 -0600

    added base file for Angular 1 testing

---
+++ public_html/github/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
+		<title>GitHub Browser</title>
+	</head>
+	<body>
+		<main class="container">
+			<h1>GitHub Browser</h1>
+		</main>
+	</body>
+</html>
