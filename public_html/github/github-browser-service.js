84c0f8dabb71848877c386fbd610114428205d7e
Dylan McDonald <dmcdonald21@cnm.edu>
2016-10-27 00:09:55 -0600

    added downloader

--- public_html/github/github-browser-service.js
+++ public_html/github/github-browser-service.js
@@ -2,6 +2,10 @@ app.service("GitHubBrowserService", function($http) {
 	this.baseUrl = "/api/github-browser/";
 
 	this.fetch = function(repository, username) {
-		return ($http.get(this.baseUrl + "?repository=" + repository + "&username=" + username));
+		return($http.get(this.baseUrl + "?repository=" + repository + "&username=" + username));
+	};
+
+	this.fetchUrl = function(url) {
+		return($http.get(url));
 	};
 });
